0167ef0433b39ed3586d742c4a202121a8a00b82
Mssj8 <juanmesiasrodriguez.est@comfandi.edu.co>
2025-10-14 18:12:43 -0500

    Add files via upload

---
+++ Lomasfresa.java
@@ -0,0 +1,30 @@
+
+package lomasfresa;
+
+/**
+ *
+ * @author Juan Manuel Mesías
+ * 
+ */
+public class Lomasfresa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String username="Camila";
+        int cofreUno=8;
+        int cofreDos=12;
+        int sumaDulces;
+        
+       sumaDulces=cofreUno+cofreDos;
+       
+       System.out.print(username + "Ahora tiene un total de " + sumaDulces + " caramelos magicos. + " );
+        
+      
+        
+    }
+    
+    
+}
---
+++ ejercicio_2 - copia.java
@@ -0,0 +1,13 @@
+
+
+/**
+ *
+ * @author Estudiante
+ */
+public class ejercicio_2 {
+    double Retouno=3.5;
+    double Retodos=4.2;
+    double Retotres=2.8;
+    
+    Promediopuntos= (Retouno+Retodos+Retotres)/3;
+}
